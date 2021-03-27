dccdae24e414284b25d6734a6471049e04fe4da8
Kit-p <kpangaa@connect.ust.hk>
2021-03-27 23:06:11 +0800

    Add a trial character with animation

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -8,6 +8,11 @@ export default class Game extends Phaser.Scene {
     preload() {
         this.load.image('tile_all-in-one-2', 'assets/tiles/all-in-one/2.png');
         this.load.tilemapTiledJSON('map_trial-01', 'assets/maps/trial-01.json');
+        this.load.atlas(
+            'atlas_all-in-one-2',
+            'assets/tiles/all-in-one/2.png',
+            'assets/tiles/atlases/all-in-one-2.json'
+        );
     }
 
     create() {
@@ -17,5 +22,18 @@ export default class Game extends Phaser.Scene {
         map.createLayer('Wall', 'tile_all-in-one-2', 0, -6);
         map.setCollisionByProperty({ collides: true });
         this.scale.setGameSize(map.widthInPixels, map.heightInPixels);
+        let elf_m = this.add.sprite(24, 32, 'atlas_all-in-one-2', 0);
+        this.anims.create({
+            key: 'elf_m_idle_anim',
+            frameRate: 4,
+            repeat: -1,
+            frames: this.anims.generateFrameNames('atlas_all-in-one-2', {
+                prefix: 'elf_m_idle_anim_f',
+                start: 0,
+                end: 3,
+                zeroPad: 1,
+            }),
+        });
+        elf_m.play('elf_m_idle_anim');
     }
 }
