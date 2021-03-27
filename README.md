15bdd294b3099db902ec6c8b89150d9545ab095f
Kit-p <kpangaa@connect.ust.hk>
2021-03-27 16:40:56 +0800

    Add a trial map

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ No Hero is a 2D dungeon RPG, built with [Phaser 3](https://phaser.io/) in JavaSc
 ## Author
 
 PANG, Kit ([Kit-p](https://github.com/Kit-p)) <kpangaa@connect.ust.hk>
+
+## Credit
+
+### tilesets
+
+-   source: <https://0x72.itch.io/dungeontileset-ii>  
+    author: [0x72](https://0x72.itch.io/)
--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -5,7 +5,17 @@ export default class Game extends Phaser.Scene {
         super('game');
     }
 
-    preload() {}
+    preload() {
+        this.load.image('tile_all-in-one-2', 'assets/tiles/all-in-one/2.png');
+        this.load.tilemapTiledJSON('map_trial-01', 'assets/maps/trial-01.json');
+    }
 
-    create() {}
+    create() {
+        let map = this.add.tilemap('map_trial-01');
+        map.addTilesetImage('tile_all-in-one-2');
+        map.createLayer('Ground', 'tile_all-in-one-2', 0, -6);
+        map.createLayer('Wall', 'tile_all-in-one-2', 0, -6);
+        map.setCollisionByProperty({ collides: true });
+        this.scale.setGameSize(map.widthInPixels, map.heightInPixels);
+    }
 }
