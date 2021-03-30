bc518a70200cd4d5546dc22f2defd4a0f2591c09
Kit-p <kpangaa@connect.ust.hk>
2021-03-31 01:12:20 +0800

    Refactor map creation code

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -3,6 +3,12 @@ import Phaser from 'phaser';
 import PlayerCharacter from '../characters/PlayerCharacter';
 
 export default class GameScene extends Phaser.Scene {
+    /** @type {{tilemap: Phaser.Tilemaps.Tilemap, layers: Phaser.Tilemaps.TilemapLayer[]}} */
+    map = {
+        tilemap: null,
+        layers: [],
+    };
+
     /** @type {Phaser.GameObjects.GameObject[]} */
     gameObjects = [];
 
@@ -29,12 +35,7 @@ export default class GameScene extends Phaser.Scene {
     }
 
     create() {
-        let map = this.add.tilemap('map_trial-1');
-        map.addTilesetImage('tile_all-in-one-2');
-        this.scale.setGameSize(map.widthInPixels, map.heightInPixels);
-        let groundLayer = map.createLayer('Ground', 'tile_all-in-one-2', 0, -6);
-        groundLayer.setCollisionByProperty({ collides: true });
-        let player = new PlayerCharacter(
+        const player = new PlayerCharacter(
             this,
             24,
             40,
@@ -42,26 +43,58 @@ export default class GameScene extends Phaser.Scene {
             'elf_m_idle_anim_f0',
             'elf_m'
         );
-        let wallLayer = map.createLayer('Wall', 'tile_all-in-one-2', 0, -6);
-        wallLayer.setCollisionByProperty({ collides: true });
-        this.physics.world.bounds.width = map.widthInPixels;
-        this.physics.world.bounds.height = map.heightInPixels;
-        this.physics.add.collider(player, groundLayer);
-        this.physics.add.collider(player, wallLayer);
-        this.cameras.main.setBounds(
-            0,
-            0,
-            map.widthInPixels,
-            map.heightInPixels
-        );
-        this.cameras.main.startFollow(player, true, 0.1, 0.1);
         this.gameObjects.push(player);
-        console.log(player);
+
+        // create the map for this scene
+        this._createMap('map_trial-1', 'tile_all-in-one-2', 0, -6);
     }
 
     update() {
         for (const gameObject of this.gameObjects) {
             gameObject.update();
         }
+    }
+
+    /**
+     * A utility method to create a map and the layers, also handles adding collisions with game objects.
+     * @param {string} tilemap The key of the tilemap.
+     * @param {string} tileset The key of the tileset image for the tilemap.
+     * @param {number} offsetX The x offset of each layer.
+     * @param {number} offsetY The y offset of each layer.
+     * @returns {Phaser.Tilemaps.Tilemap} The created map.
+     */
+    _createMap(tilemap, tileset, offsetX = 0, offsetY = 0) {
+        const map = this.add.tilemap(tilemap);
+        map.addTilesetImage(tileset);
+        this.scale.setGameSize(map.widthInPixels, map.heightInPixels);
+        this.physics.world.bounds.width = map.widthInPixels;
+        this.physics.world.bounds.height = map.heightInPixels;
+        let layerDepth = 0;
+        for (const layerData of map.layers) {
+            const layer = map.createLayer(
+                layerData.name,
+                tileset,
+                offsetX,
+                offsetY
+            );
+            // add layer for collision detection
+            layer.setCollisionByProperty({ collides: true }, true);
+            // set the depth according to the layer's order
+            layer.setDepth(layerDepth);
+            layerDepth += 10;
+            // allow game objects to collide with layer
+            for (const gameObject of this.gameObjects) {
+                this.physics.add.collider(gameObject, layer);
+            }
+            this.map.layers.push(layer);
+        }
+        // ensure all sprites are at least on top of the lowest layer
+        for (const gameObject of this.gameObjects) {
+            if (gameObject instanceof Phaser.GameObjects.Sprite) {
+                gameObject.setDepth(this.map.layers[0].depth + 1);
+            }
+        }
+        this.map.tilemap = map;
+        return map;
     }
 }
